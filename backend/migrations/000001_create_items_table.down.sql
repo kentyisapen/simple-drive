c37fe4e08c887795eb663ee133f8b4e14a4f4040
kentyisapen <kenkenpoco@gmail.com>
2024-04-13 09:45:35 +0900

    :card_file_box: マイグレーションファイルのupとdownが逆になっていたのを修正

--- backend/migrations/000001_create_items_table.down.sql
+++ backend/migrations/000001_create_items_table.down.sql
@@ -1,12 +1,3 @@
-CREATE TYPE item_type AS ENUM ('FILE', 'DIRECTORY');
+DROP TABLE IF EXISTS items;
 
-CREATE TABLE items (
-    id UUID PRIMARY KEY,
-    parent_id UUID NULL,
-    thumbnail_id UUID NULL,
-    name VARCHAR(255) NOT NULL,
-    type item_type NOT NULL,
-    size BIGINT NULL,
-    created_at TIMESTAMP NULL,
-    last_modified_at TIMESTAMP NULL
-);
+DROP TYPE IF EXISTS item_type;
--- backend/migrations/000001_create_items_table.up.sql
+++ backend/migrations/000001_create_items_table.up.sql
@@ -1,3 +1,5 @@
-DROP TABLE IF EXISTS items;
+CREATE TYPE item_type AS ENUM('FILE', 'DIRECTORY');
 
-DROP TYPE IF EXISTS item_type;
+CREATE TABLE items (
+    id UUID PRIMARY KEY, parent_id UUID NULL, thumbnail_id UUID NULL, name VARCHAR(255) NOT NULL, type item_type NOT NULL, size BIGINT NULL, created_at TIMESTAMP NULL, last_modified_at TIMESTAMP NULL
+);

7594a4902e99dec2b6708b23836f1aff8fd0b58d
kentyisapen <kenkenpoco@gmail.com>
2024-04-24 02:14:26 +0900

    fix: settings

--- frontend/src/features/item/ItemBox/ItemBoxContainer.tsx
+++ frontend/src/features/item/ItemBox/ItemBoxContainer.tsx
@@ -1,11 +1,11 @@
-import { Item } from "@/grpc/generated/simple-drive_pb"
-import { ItemBoxPresenter } from "./ItemBoxPresenter"
+import { Item } from "@/grpc/generated/simple-drive_pb";
+import { ItemBoxPresenter } from "./ItemBoxPresenter";
 
 export type Props = {
-    item: Item.AsObject
-    handleOnClickItem: (item: Item.AsObject) => void
-}
+	item: Item.AsObject;
+	handleOnClickItem: (item: Item.AsObject) => void;
+};
 
 export const ItemBoxContainer = (props: Props) => {
-    return ItemBoxPresenter(props)
-}
+	return ItemBoxPresenter(props);
+};
--- frontend/src/utils/fetchItemBinary.ts
+++ frontend/src/utils/fetchItemBinary.ts
@@ -1,22 +1,24 @@
 type Result =
-    | {
-        data: ArrayBuffer;
-        error: null;
-    }
-    | {
-        data: null;
-        error: Error;
-    };
+	| {
+			data: ArrayBuffer;
+			error: null;
+	  }
+	| {
+			data: null;
+			error: Error;
+	  };
 
 export const fetchItemBinary = async (itemId: string): Promise<Result> => {
-    try {
-        const response = await fetch(process.env.NEXT_PUBLIC_BASE_MINIO_OBJECT_URL + itemId)
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const data = await response.arrayBuffer();
-        return { data, error: null };
-    } catch (e) {
-        return { data: null, error: e instanceof Error ? e : new Error(String(e)) };
-    }
-}
+	try {
+		const response = await fetch(
+			process.env.NEXT_PUBLIC_BASE_MINIO_OBJECT_URL + itemId
+		);
+		if (!response.ok) {
+			throw new Error(`HTTP error! status: ${response.status}`);
+		}
+		const data = await response.arrayBuffer();
+		return { data, error: null };
+	} catch (e) {
+		return { data: null, error: e instanceof Error ? e : new Error(String(e)) };
+	}
+};
